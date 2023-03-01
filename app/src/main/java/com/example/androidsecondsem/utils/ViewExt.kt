c5356b14b82d0fbdb1779a53d453af8fbbfc040b
Динара <zaripovadin@yandex.ru>
2023-03-01 12:54:10 +0300

    Homework ready

--- app/src/main/java/com/example/androidsecondsem/utils/ViewExt.kt
+++ app/src/main/java/com/example/androidsecondsem/utils/ViewExt.kt
@@ -3,22 +3,6 @@ package com.example.androidsecondsem.utils
 import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import androidx.annotation.StringRes
-import com.google.android.material.snackbar.Snackbar
-
-fun View.showSnackbar(
-    @StringRes message: Int,
-    duration: Int = Snackbar.LENGTH_SHORT
-) = Snackbar
-    .make(this, message, duration)
-    .show()
-
-fun View.showSnackbar(
-    message: CharSequence,
-    duration: Int = Snackbar.LENGTH_SHORT
-) = Snackbar
-    .make(this, message, duration)
-    .show()
 
 fun View.hideKeyboard() {
     (context.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager)
